5f9d7569adc744ef29b6bb6dbfc02213f03480f3
Abukuii <abukuiabuku@gmail.com>
2024-11-06 01:09:48 +0500

    malumot qo'shimcha jsda ishlandi

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const hamburgerBtn = document.querySelector("#hamburger-btn");
     const closeMenuBtn = document.querySelector("#close-menu-btn");
     let section = document.getElementById('section');
+    const table = document.querySelector(".tbody")
 
     // Toggle mobile menu on hamburger button click
     hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
@@ -11,7 +12,36 @@ window.addEventListener('DOMContentLoaded', () => {
     // Close mobile menu on close button click
     closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
+    const Mahsulotlar = {
+        tovarlar: [
+            'Olma',
+            'Anor',
+            'Behi',
+            'Olcha',
+        ],
+    }
     
+    table.innerHTML = ' '
+
+    Mahsulotlar.tovarlar.forEach((item, idx) => {
+        table.innerHTML += `
+            <tr>
+                <td>${idx + 1}</td>
+                <!-- <td><img src="../img/img1.jpg" alt=""> </td> -->
+                <td class="name">${item}</td>
+                <td class="name">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nisi sit libero dolor molestias corporis laborum nesciunt. Odit, sed dolorem quidem molestiae nobis ipsam, debitis necessitatibus ex eveniet, fugiat quos?</td>
+                <td>25.04.2024</td>
+                <td>100</td>
+                <td onclick="rename()" class="pen">
+                    <i class='bx bxs-pencil'></i>
+                </td>
+                <td onclick="del()">
+                    <i class='bx bxs-trash-alt'></i>
+                </td>
+            </tr>
+        `
+    })
+
 
 
 })
